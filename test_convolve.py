14b25ee3d3aafdf832d5c690fb0712ea9e171137
ddokupil12 <redder.surgery0e@icloud.com>
2025-04-26 11:55:53 -0500

    Revised test file

--- test_convolve.py
+++ test_convolve.py
@@ -1,7 +1,30 @@
 import models
-import torch
+from torch import tensor, sum, zeros, randn
 
-a = torch.randn(4, 5)
-b = torch.randn(2, 2)
+a = randn(4, 5)
+b = randn(2, 2)
 
 print(models.Convolve(a, b))
+
+
+def Convolve(input: tensor, weight: tensor):
+    input_tensor_dimensions = input.shape
+    weight_dimensions = weight.shape
+    Output_Tensor = tensor(())
+    print('output...')
+    inputHeight, inputWidth = input_tensor_dimensions
+    weightHeight, weightWidth = weight_dimensions
+    outputHeight = (inputHeight - weightHeight)
+    outputWidth = (inputWidth - weightWidth)
+
+    Output_Tensor = zeros(outputHeight, outputWidth)
+
+    for i in range(outputHeight):
+        for k in range(outputWidth):
+            window = input[i:i + weightHeight, k:k + weightWidth]
+            Output_Tensor[i, k] = sum(window * weight)
+
+
+    print(Output_Tensor.shape)
+    print(Output_Tensor)
+    return Output_Tensor
